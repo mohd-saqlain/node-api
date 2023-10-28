6282140470d0967c44cfe4067b8f03989d9af0cc
Shane Alam <shanealam6123@gmail.com>
2023-10-28 16:50:28 +0530

    duty

--- db/connect.js
+++ db/connect.js
@@ -5,4 +5,4 @@ const url = 'mongodb+srv://saqlainaly:w59rhThu4142wuAs@cluster0.nixfrcw.mongodb.
 mongoose.connect(url,{
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(res=>console.log("Mongo Connected")).catch(err=>console.log(err))
+}).then(res=>console.log("Mongo Connected to db")).catch(err=>console.log(err))
